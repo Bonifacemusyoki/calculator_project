8f89e85732e713b7a4033ca4816451fafc270ee7
Bonifacemusyoki <124658111+Bonifacemusyoki@users.noreply.github.com>
2023-02-15 00:39:32 -0800

    added calculator functions

--- calculator.py
+++ calculator.py
@@ -1,22 +1,41 @@
 number1= eval(input("Enter first number:"))
-number2= eval(inout("Enter first number:"))
-operator= input("Enter operator")
+number2= eval(input("Enter second number:"))
+operator= input("Enter operator:")
 
 
 def add(num1,num2):
     result=num1 + num2
-    return result
+    print(result)
 
 
 def subtract(num1,num2):
     result=num1 - num2
-    return result
+    print(result)
 
 
 def multiply(num1,num2):
     result=num1 * num2
-    return result
+    print(result)
 
 def divide(num1,num2):
     result=num1 / num2
-    return result
+    print(result)
+
+
+if operator=="+":
+    add(number1,number2)
+
+elif operator=="-":
+    subtract(number1,number2)
+
+elif operator=="/":
+    divide(number1,number2)
+
+
+
+elif operator=="*":
+    multiply(number1,number2)
+
+else:
+    print("invalid operator")
+    
